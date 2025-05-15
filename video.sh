25e8df4322dfbb01f797cbde04575f3a59e6c578
Luis Garcia <git@luigi311.com>
2025-05-15 17:54:39 -0600

    Videos: Use mp4 for browser support
    Signed-off-by: Luis Garcia <git@luigi311.com>

--- video.sh
+++ video.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -c:a copy Short_1080p.mkv
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -c:v libvpx-vp9 -c:a copy Short_1080p_vp9.mkv
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -c:v libx265 -preset medium -c:a copy Short_1080p_hevc.mkv
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -c:v libaom-av1 -cpu-used 5 -c:a copy Short_1080p_av1.mkv
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -c:a copy Short_1080p.mp4
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -c:v libvpx-vp9 -c:a copy Short_1080p_vp9.mp4
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -c:v libx265 -preset medium -c:a copy Short_1080p_hevc.mp4
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -c:v libaom-av1 -cpu-used 5 -c:a copy Short_1080p_av1.mp4
 
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:720 -c:a copy Short_720p.mkv
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:720 -c:v libvpx-vp9 -c:a copy Short_720p_vp9.mkv
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:720 -c:v libx265 -preset medium -c:a copy Short_720p_hevc.mkv
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:720 -c:v libaom-av1 -cpu-used 5 -c:a copy Short_720p_av1.mkv
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:720 -c:a copy Short_720p.mp4
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:720 -c:v libvpx-vp9 -c:a copy Short_720p_vp9.mp4
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:720 -c:v libx265 -preset medium -c:a copy Short_720p_hevc.mp4
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:720 -c:v libaom-av1 -cpu-used 5 -c:a copy Short_720p_av1.mp4
 
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:576 -c:a copy Short_576p.mkv
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:576 -c:v libvpx-vp9 -c:a copy Short_576p_vp9.mkv
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:576 -c:v libx265 -preset medium -c:a copy Short_576p_hevc.mkv
-ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:576 -c:v libaom-av1 -cpu-used 5 -c:a copy Short_576p_av1.mkv
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:576 -c:a copy Short_576p.mp4
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:576 -c:v libvpx-vp9 -c:a copy Short_576p_vp9.mp4
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:576 -c:v libx265 -preset medium -c:a copy Short_576p_hevc.mp4
+ffmpeg -y -ss 00:07:00 -t 00:00:30 -i "$1" -filter:v scale=-1:576 -c:v libaom-av1 -cpu-used 5 -c:a copy Short_576p_av1.mp4
